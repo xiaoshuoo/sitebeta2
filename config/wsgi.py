5fa69974ff8df195b86b3e11ab65a0f585397d4e
Xiaoshuoo <136738645+xiaoshuoo@users.noreply.github.com>
2024-11-19 18:18:06 +0300

    Initial commitжд

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,8 +1,13 @@
 import os
+import sys
+from pathlib import Path
+
+# Добавляем путь к проекту
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 from django.core.wsgi import get_wsgi_application
 
-# Устанавливаем настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Получаем WSGI приложение
 application = get_wsgi_application() 
