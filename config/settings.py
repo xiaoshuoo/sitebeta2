0b7f690fc7e3e3c4ecf86bdb3ff32bda01645b13
Xiaoshuoo <136738645+xiaoshuoo@users.noreply.github.com>
2024-11-19 18:20:20 +0300

    Initial commitждэ

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,36 @@ REQUIRED_DIRS = [
 
 for directory in REQUIRED_DIRS:
     os.makedirs(directory, exist_ok=True)
+
+# Добавляем настройки TinyMCE
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 'auto',
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'silver',
+    'plugins': '''
+        textcolor save link image media preview codesample contextmenu
+        table code lists fullscreen insertdatetime nonbreaking
+        contextmenu directionality searchreplace wordcount visualblocks
+        visualchars code fullscreen autolink lists charmap print hr
+        anchor pagebreak
+        ''',
+    'toolbar1': '''
+        fullscreen preview bold italic underline | fontselect,
+        fontsizeselect | forecolor backcolor | alignleft alignright |
+        aligncenter alignjustify | indent outdent | bullist numlist table |
+        | link image media | codesample |
+        ''',
+    'toolbar2': '''
+        visualblocks visualchars |
+        charmap hr pagebreak nonbreaking anchor | code |
+        ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+}
 
 
 
--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,6 @@ from blog.views import register
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('tinymce/', include('tinymce.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         template_name='registration/logged_out.html',
@@ -27,15 +26,8 @@ urlpatterns = [
     path('reset/done/', 
          auth_views.PasswordResetCompleteView.as_view(), 
          name='password_reset_complete'),
-    path('password_change/', 
-         auth_views.PasswordChangeView.as_view(), 
-         name='password_change'),
-    path('password_change/done/', 
-         auth_views.PasswordChangeDoneView.as_view(), 
-         name='password_change_done'),
 ]
 
-# Добавляем обработку медиа-файлов для режима разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
